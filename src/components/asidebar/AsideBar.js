b503dd5c45766704c82d5e3aa60879853f523cf7
principalkelvo <principalkelvo@gmail.com>
2022-10-25 13:58:17 +0300

    imports from pro side bar component collapses on click and has breakpoint

--- src/components/asidebar/AsideBar.js
+++ src/components/asidebar/AsideBar.js
@@ -1,15 +1,37 @@
-import React, { useState } from "react";
+import React from "react";
+// import AsideBarCSS from "./AsideBar.module.css";
+import { Sidebar, Menu, MenuItem, useProSidebar } from "react-pro-sidebar";
+import { Link } from "react-router-dom";
+import LoginForm from "../LoginForm";
 
 function AsideBar() {
-  const [open, setOpen] = useState(true);
+  // const [open, setOpen] = useState(true);
+  const { collapseSidebar, toggleSidebar, broken } = useProSidebar();
   return (
-    <>
-      <div class="sidebar">
-        <ul class="nav-links">
-        <span class="link_name">Dashboard</span>
-        </ul>
-      </div>
-    </>
+    <div style={{ display: "flex", height: "100%", minHeight: "400px" }}>
+      <Sidebar customBreakPoint="700px" transitionDuration={1000}>
+        <Menu>
+          <MenuItem routerLink={<Link to="/dashboard" />}> Dashboard</MenuItem>
+          <MenuItem routerLink={<Link to="/calendar" />}> Calendar</MenuItem>
+          <MenuItem routerLink={<Link to="/e-commerce" />}>
+            {" "}
+            E-commerce
+          </MenuItem>
+        </Menu>
+      </Sidebar>
+      <main style={{ padding: "10" }}>
+        <div>
+          {broken ? (
+            <button className="sb-button" onClick={() => toggleSidebar()}>
+              Toggle
+            </button>
+          ) : (
+            <button onClick={() => collapseSidebar()}>Collapse</button>
+          )}
+        </div>
+        <LoginForm />
+      </main>
+    </div>
   );
 }
 export default AsideBar;
