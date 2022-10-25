7b2e0683157149d9e4d718c91a7b2e5c03baf5c0
principalkelvo <principalkelvo@gmail.com>
2022-10-25 13:57:17 +0300

    accepts a sidebar

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AsideBar from "../asidebar/AsideBar";
+// import LoginForm from "../LoginForm";
+function Dashboard() {
+  return (
+    <>
+      <AsideBar/>
+      {/* <LoginForm/> */}
+    </>
+  );
+}
+export default Dashboard;
