c5e8dc35963b59ebac1363cc3eec870c7c14c59a
principalkelvo <principalkelvo@gmail.com>
2022-10-25 13:59:55 +0300

    checks if user is available  if not it shows the dashboard  this helps in running my part of viewing without the backend  later we will change to if user is admin

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
 import Login from "./pages/Login";
+import { ProSidebarProvider } from "react-pro-sidebar";
+import Dashboard from "./components/dashboard/Dashboard";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -14,12 +16,22 @@ function App() {
       }
     });
   }, []);
+  // a trial to check if user exists 
+  // i am using it to view the dashboard page 
+  if (!user)
+    return (
+      <ProSidebarProvider>
+        <Dashboard />
+      </ProSidebarProvider>
+    );
 
   return (
-  <div className="App">
-    <NavBar />
-    <Login />
-  </div>);
+    <div className="App">
+      <NavBar />
+
+      <Login />
+    </div>
+  );
 }
 
 export default App;
