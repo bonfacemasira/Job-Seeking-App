a890f491b49d8e92019cbd0d4fba12ded1b06ae7
principalkelvo <principalkelvo@gmail.com>
2022-10-25 20:31:47 +0300

    creates columns for data

---
+++ src/components/table/column.js
@@ -0,0 +1,29 @@
+// columns.js
+// Header – this is the column's name that will be displayed to help identify each column.
+// Accessor – this is the key in our data. This is what will be used to assign each value to a column.
+export const COLUMNS = [
+    {
+        Header: 'ID',
+        accessor: 'id_number',
+    },
+    {
+        Header: 'First Name',
+        accessor: 'firstName',
+    },
+    {
+        Header: 'Last Name',
+        accessor: 'lastName',
+    },
+    {
+        Header: 'Email',
+        accessor: 'email',
+    },
+    {
+        Header: 'Department',
+        accessor: 'department',
+    },
+    {
+        Header: 'Date Joined',
+        accessor: 'dateJoined',
+    },
+];
