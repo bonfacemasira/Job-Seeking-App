18c22bb06c32c070728ba0d92d9de4f467332671
principalkelvo <principalkelvo@gmail.com>
2022-10-25 20:32:52 +0300

    creates table from react table library the table uses fake data from hygraph

---
+++ src/components/table/Table.js
@@ -0,0 +1,91 @@
+import { useState, useEffect, useMemo } from "react";
+import { useTable,useGlobalFilter } from "react-table";
+import { COLUMNS } from "./components/columns";
+
+import request from "graphql-request";
+
+function Table() {
+  const [data, setData] = useState([]);
+
+  // useeffect fake data...
+  useEffect(() => {
+    const fetchData = async () => {
+      const { datasets } = await request(
+        "https://api-us-east-1.hygraph.com/v2/cl6o3qnz40n6401uje6140m78/master",
+        `
+                {
+                    datasets (first:50) {
+                        id_number
+                        firstName
+                        lastName
+                        email
+                        department
+                        dateJoined
+                        }
+                }
+                `
+      );
+      setData(datasets);
+    };
+    fetchData();
+  }, []);
+
+  const columns = useMemo(() => COLUMNS, []);
+  // const tableInstance = useTable({ columns, data });
+  // From the tableInstance, the hook will return the necessary props for the table, body, and transformed data to create the header and cells for our table. Let's destructure directly to access these props:
+//   The useGlobalFilter allows us to filter the entire table based on any value passed into our filter text field.
+  const {   getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    state,
+    setGlobalFilter,
+    prepareRow,} =
+    useTable({ columns, data }, useGlobalFilter);
+
+    // destructure globalFilter from the state method
+    const { globalFilter } = state;
+
+    
+    return (
+        <div>
+            <div className="search-container">
+                <input
+                    type="text"
+                    value={globalFilter || ''}
+                    onChange={(e) => setGlobalFilter(e.target.value)}
+                />
+            </div>
+            <table {...getTableProps()}>
+                <thead>
+                    {headerGroups.map((headerGroup) => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map((column) => (
+                                <th {...column.getHeaderProps()}>
+                                    {column.render('Header')}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {rows.map((row) => {
+                        prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map((cell) => {
+                                    return (
+                                        <td {...cell.getCellProps()}>
+                                            {cell.render('Cell')}
+                                        </td>
+                                    );
+                                })}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+export default Table;
