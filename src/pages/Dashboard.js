587f213bfebf298ae14e73c043f2405c374ec102
principalkelvo <principalkelvo@gmail.com>
2022-10-26 20:48:15 +0300

    creates a dashboard to view employers and job seekers joining rate has images for successful employers

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,41 @@
 import React from "react";
-import Table from "../components/table/Table";
-// import LoginForm from "../LoginForm";
+import DashboardCSS from "./Dashboard.module.css";
 function Dashboard() {
+  //fetch data from backend
+  //comment out the image border on the images
   return (
     <>
-      <Table/>
-      {/* <LoginForm/> */}
+      <div className={DashboardCSS.main}>
+        <h1>Overview</h1>
+        <div className={DashboardCSS.container}>
+          <div className={DashboardCSS.column6}>
+            <div className={DashboardCSS.fullwidth}>
+              <div>JobSeekers</div>
+              <div className={DashboardCSS.rate}>8%</div>
+            </div>
+            <div className={DashboardCSS.fullwidth}>
+              <span>10,837</span>
+            </div>
+          </div>
+          <div className={DashboardCSS.column6}>
+            <div className={DashboardCSS.fullwidth}>
+              <div>Employers</div>
+              <div className={DashboardCSS.rate}>8%</div>
+            </div>
+            <div className={DashboardCSS.fullwidth}>
+              <span>10,837</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={DashboardCSS.main}>
+        <h1>Todays top employers</h1>
+        <div className={DashboardCSS.images}>
+          <div className={DashboardCSS.image}>
+            <img src="#" alt="#" />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
