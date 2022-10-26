286011a2c9023836bfc39732ed76da884aeec702
Bonface Masira <bonfacemasira@gmail.com>
2022-10-26 18:47:28 +0300

    create dashboad and profile pages

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ function App() {
   return (
   <div className="App">
     <NavBar />
-    {/* <Login /> */}
+    <Login />
     {/* <IdentityForm /> */}
-    <VerificationForm />
+    {/* <VerificationForm /> */}
   </div>);
 }
 
