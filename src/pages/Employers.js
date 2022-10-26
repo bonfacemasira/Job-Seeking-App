d806acb6f84332c08b8a51032e9f0ca1735c5e52
principalkelvo <principalkelvo@gmail.com>
2022-10-26 20:49:04 +0300

    cleaning to view

--- src/pages/Employers.js
+++ src/pages/Employers.js
@@ -1,4 +1,4 @@
-import React from " react";
+import React from "react";
 import Table from "../components/table/Table";
 
 function Employers() {
--- src/pages/JobSeekers.js
+++ src/pages/JobSeekers.js
@@ -1,7 +1,9 @@
-import React from " react";
+import React from "react";
 import Table from "../components/table/Table";
 
 function JobSeekers() {
+    //fetch data from backend give it a variable of data and columns and pass it to tables
+
   return (
     <>
       <Table />
