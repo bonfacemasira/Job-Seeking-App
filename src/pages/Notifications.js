d808039813473733012da27234d6bea4eaa7985a
principalkelvo <principalkelvo@gmail.com>
2022-11-01 01:56:31 +0300

    display all notifications

---
+++ src/pages/Notifications.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Notification from "../components/notification/Notification";
+
+function Employers() {
+  return (
+    <>
+      <Notification />
+    </>
+  );
+}
+export default Employers;
