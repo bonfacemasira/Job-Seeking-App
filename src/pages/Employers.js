736b836adc59bd667f49bcf0de51a3c8bd0377cd
principalkelvo <principalkelvo@gmail.com>
2022-11-01 00:42:42 +0300

    gets data from employers and sends it to the table

--- src/pages/Employers.js
+++ src/pages/Employers.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import Table from "../components/table/Table";
 
 function Employers() {
+  const [employers, setEmployers] = useState([]);
+
+  useEffect(() => {
+    // auto-login
+    fetch("http://127.0.0.1:3000/employers").then((r) => {
+      if (r.ok) {
+        r.json().then((employer) => setEmployers(employer));
+      }
+    });
+  }, []);
+  console.log("employer", employers);
+  const employersData = useMemo(() => [...employers], [employers]);
+  console.log("employersData", employersData);
+  const employersColumns = useMemo(
+    () =>
+      employers[0]
+        ? Object.keys(employers[0])
+            .filter((key) => key !== "user")
+            .map((key) => {
+              return { Header: key, accessor: key };
+            })
+        : [],
+    [employers]
+  );
+  console.log("employersColumns", employersColumns);
   return (
     <>
-      <Table />
+      <Table columns={employersColumns} data={employersData} />
     </>
   );
 }
