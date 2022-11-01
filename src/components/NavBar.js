b5aa2ef260a2e59b9eea690393a28ea744af1f96
principalkelvo <principalkelvo@gmail.com>
2022-11-01 13:41:45 +0300

    creates a navbar that checks if user is available

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,43 @@
 import "../App.css";
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell } from "@fortawesome/free-solid-svg-icons";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 function NavBar() {
+  //add user as a prop
+  const user = "1";
+
+  
   return (
     <div className="wrapper">
       <h1 className="logo">JS</h1>
+
       <nav className="Nav">
-        <h1>Home</h1>
-        <h1>Find Talent</h1>
-        <h1>Employers</h1>
-        <Link to="/login" className="navButton">
-          Login
-        </Link>
-        <Link to="/register" className="navButton">
-          Sign Up
-        </Link>
+        {!user ? (
+          <>
+            <Link to="/login" className="navButton">
+              Login
+            </Link>
+            <Link to="/register" className="navButton">
+              Sign Up
+            </Link>
+          </>
+        ) : (
+          <>
+            <div className="notifications">
+              <FontAwesomeIcon icon={faBell} />
+              <div className="total_notifications">9</div>
+            </div>
+            <div className="user_prof">
+              <div className="user_image">
+                <img src="#" alt="prof" />
+              </div>
+              <FontAwesomeIcon icon={faChevronDown} />
+            </div>
+          </>
+        )}
       </nav>
     </div>
   );
